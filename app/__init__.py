9efe8f5f65381c4881b375892d327bedb341272b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-06-26 17:39:11 -0500

    added dashboard route

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -14,5 +14,6 @@ def create_app(test_config=None):
     return 'hello world'
 
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
