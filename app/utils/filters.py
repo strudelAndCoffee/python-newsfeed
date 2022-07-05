cbbfaa3c5b742e45821b0a1c4998fb4ed2307535
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-04 22:57:47 -0500

    created format functions in utils

---
+++ app/utils/filters.py
@@ -0,0 +1,14 @@
+def format_date(date):
+  return date.strftime('%m/%d/%y')
+
+def format_url(url):
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+def format_plural(amount, word):
+  if amount != 1:
+    return word + 's'
+
+  return word
+
+print(format_plural(2, 'cat'))
+print(format_plural(1, 'dog'))
