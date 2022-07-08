30033720b9b5d8a397693d5584a376adf8e4686a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-08 16:29:39 -0500

    added error response for login

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,13 +10,17 @@ def signup():
   data = request.get_json()
   db = get_db()
 
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
-
-  db.add(newUser)
-  db.commit()
+  try:
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
+  
+    db.add(newUser)
+    db.commit()
+  except:
+    print(sys.exe_info()[0])
+    return jsonify(message = 'Signup failed'), 500
 
   return jsonify(id = newUser.id)
